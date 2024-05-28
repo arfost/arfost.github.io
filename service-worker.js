8834bfa1b910335ae8d8281ae71f2728e5e1cea2
arfost <chevalier.frugier@gmail.com>
2024-05-29 00:04:27 +0200

    test sw

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// service-worker.js
+
+// Register the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('ADR-v1').then(cache => {
+      return cache.addAll([
+        // Add your static assets here
+        '/',
+        '/index.html',
+        '/modules/Dice.js',
+        '/modules/Roll.js',
+        '/modules/Game.js',
+        '/assets/dice/d4.png',
+        '/assets/dice/d6.png',
+        '/assets/dice/d8.png',
+        '/assets/dice/d10.png',
+        '/assets/dice/d12.png',
+        '/assets/dice/d20.png',
+        '/start.js'
+      ]);
+    })
+  );
+});
+
+// Intercept fetch requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- start.js
+++ start.js
@@ -1,6 +1,17 @@
 import { Game } from './modules/Game.js';
 // import { DefaultMapLoader } from './modules/MapLoader.js';
 
+//register the service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(reg => {
+      console.log('Service worker registered');
+    })
+    .catch(err => {
+      console.log('Service worker registration failed: ', err);
+    });
+}
+
 async function initGame() {
 
   const display = document.getElementById('game');
